51a6ba6fb7d544035d1ec727c8540eeaa1b916ed
Jovy83 <jovy83@live.com>
2016-12-12 12:43:29 +0800

    Added custom cell for the collection view. Done setting up delegate methods for collection view

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,42 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Jovy Ong on 12/12/16.
+//  Copyright © 2016 DIGIGAMES INTERACTIVE. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    //MARK: OUTLETS
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    //MARK: PROPERTIES
+    var pokemon: Pokemon!
+    
+    //MARK: METHODS
+    
+    // You can either use awakeFromNib or required init? for the initialization of this cell
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+//    override func awakeFromNib() {
+//        layer.cornerRadius = 5.0
+//    }
+    
+    func configureCell(pokemon: Pokemon)
+    {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -8,16 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    
+    @IBOutlet weak var collection: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
-        
+        setupCollectionView()
         
     }
+    
+    // MARK: MY FUNCTIONS
+    func setupCollectionView()
+    {
+        collection.delegate = self
+        collection.dataSource = self
+    }
 
+    //MARK: COLLECTION VIEW DELEGATE METHODS
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // similar to cell for row for tableviews
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell
+        {
+            //cell.configureCell(pokemon: <#T##Pokemon#>)
+            return cell
+        }
+        
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // called when an item in the collection is tapped
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // the number of items
+        return 30
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // the number of sections
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // we set the size of the cell to the same size we set in the storyboard
+        return CGSize(width: 105, height: 105)
+    }
 
 }
 
